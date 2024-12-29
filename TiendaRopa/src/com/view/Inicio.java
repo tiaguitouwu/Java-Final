06f2bc5a69e04454fc2ca53163768473ba452047
Tiago <87438400+tiaguitouwu@users.noreply.github.com>
2024-12-29 05:22:37 -0300

    Add pantalla principal

--- TiendaRopa/src/com/view/Inicio.java
+++ TiendaRopa/src/com/view/Inicio.java
@@ -4,6 +4,8 @@
  */
 package com.view;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author tiago
@@ -13,7 +15,9 @@ public class Inicio extends javax.swing.JFrame {
     /**
      * Creates new form Inicio
      */
+    
     public Inicio() {
+        
         initComponents();
     }
 
@@ -26,57 +30,129 @@ public class Inicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        cargaVenta = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        cargarCliente = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        cargarCompra = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        cargarStock = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem6 = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setForeground(java.awt.Color.white);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/resources/fondoApp2.jpg"))); // NOI18N
+
+        jMenu1.setLabel("Venta");
+
+        cargaVenta.setText("Pedidos");
+        cargaVenta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cargaVentaActionPerformed(evt);
+            }
+        });
+        jMenu1.add(cargaVenta);
+
+        jMenuItem4.setText("Factura");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem4);
+
+        cargarCliente.setText("Clientes");
+        jMenu1.add(cargarCliente);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setLabel("Compra");
+
+        cargarCompra.setText("Pedidos");
+        cargarCompra.setActionCommand("Pedido");
+        jMenu2.add(cargarCompra);
+
+        jMenuItem5.setText("Factura");
+        jMenu2.add(jMenuItem5);
+
+        jMenuItem1.setText("Proveedores");
+        jMenu2.add(jMenuItem1);
+
+        jMenuBar1.add(jMenu2);
+
+        cargarStock.setLabel("Inventario");
+
+        jMenuItem3.setText("Stock");
+        cargarStock.add(jMenuItem3);
+
+        jMenuItem2.setText("Articulos");
+        cargarStock.add(jMenuItem2);
+
+        jMenuBar1.add(cargarStock);
+
+        jMenu3.setText("Gestión Tiendas");
+
+        jMenuItem6.setText("Sucursales");
+        jMenu3.add(jMenuItem6);
+
+        jMenuBar1.add(jMenu3);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 588, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 446, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void cargaVentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cargaVentaActionPerformed
+        CargarPedidoVenta pedidoVenta = new CargarPedidoVenta();
+        pedidoVenta.show();
+    }//GEN-LAST:event_cargaVentaActionPerformed
+
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        CargarFacturaVenta facturaVenta = new CargarFacturaVenta();
+        facturaVenta.show();
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Inicio().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem cargaVenta;
+    private javax.swing.JMenuItem cargarCliente;
+    private javax.swing.JMenuItem cargarCompra;
+    private javax.swing.JMenu cargarStock;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
     // End of variables declaration//GEN-END:variables
 }
